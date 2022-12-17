10fcfc7ce34b5745c8dfcff5c755c3f6dd5af4ac
Jake Thompson <jake@zenlist.com>
2022-12-17 16:14:08 -0500

    Cleaup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,22 +8,19 @@ export default function Document() {
       <Head>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
         <Script
-          async
-          strategy="afterInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=G-2FDB7X0B4R"
+          strategy="afterInteractive"
         />
 
-        <Script
-          id="gtm"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
         
-          gtag('config', 'G-2FDB7X0B4R');`,
-          }}
-        ></Script>
+            gtag('config', 'G-2FDB7X0B4R');
+          `}
+        </Script>
       </Head>
       <body className="bg-white">
         <Main />
