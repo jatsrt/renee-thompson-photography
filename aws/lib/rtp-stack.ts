3cb79afffb22156c93593522d63bb9e837a5a39a
Jake Thompson <jake@zenlist.com>
2024-04-19 08:42:27 -0400

    Version updates

--- aws/lib/rtp-stack.ts
+++ aws/lib/rtp-stack.ts
@@ -2,8 +2,6 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { GalleryConstruct } from "./gallery-construct";
 
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
 export class RtpStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
